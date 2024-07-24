e519805a752b6c2d519c470a311ac778283d0ebc
root <St>
2024-07-24 23:02:52 +0000

    0

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,4 +1,5 @@
-import getResponseFromAPI from './0-promise.js';
+import getResponseFromAPI from "./0-promise.js";
 
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
+
