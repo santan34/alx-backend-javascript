c167b7d5fc49b105c4cf05d84e7d84a0900a6958
St <aaronndas@gmail.com>
2024-07-26 02:09:24 +0200

    00

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,12 @@
+/**
+ * Exports a classroom class
+ */
 export default class ClassRoom {
   constructor(maxStudentsSize) {
+      /**
+       * creates a new ClassRom class
+       * @maxStudentsSize -  the size of the students number
+       */
       this._maxStudentsSize = maxStudentsSize;
     }
 }
