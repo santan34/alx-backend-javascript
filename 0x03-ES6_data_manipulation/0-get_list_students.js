d3133f388b7604436b12cf9b0f79c2d7f2e348ee
St <aaronndas@gmail.com>
2024-08-01 01:12:24 +0200

    000o000

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,7 @@
-export default function getListStudents() {
-  const list = [];
-  const obj1 = {
-    id: 1,
-    firstname: 'Guillaume',
-    location: 'San Francisco',
-  };
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
 
-  const obj2 = {
-    id: 2,
-    firstname: 'James',
-    location: 'Columbia',
-  };
-
-  const obj3 = {
-    id: 5,
-    firstname: 'Serena',
-    location: 'San Francisco',
-  };
-
-  list.push(obj1, obj2, obj3);
-  return list;
-}
+export default getListStudents;
