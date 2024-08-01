6a7c3b1460d2dcb47d997f9703ca76a3020b0321
St <aaronndas@gmail.com>
2024-08-01 02:28:05 +0200

    test 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,14 @@
 export default function updateStudentGradeByCity(listofstudents, city, newGrades) {
   const filtered = listofstudents.filter((elem) => elem.location === city);
-  return filtered;
+  const returned = filtered.map((student) => {
+    const newGrade = newGrades.find((gradeobj) => gradeobj.studentId === student.id);
+    const newObj = student;
+    if (newGrade) {
+      newObj.grade = newGrade.grade;
+    } else {
+      newObj.grade = 'Nan';
+    }
+    return newObj;
+  });
+  return returned;
 }
