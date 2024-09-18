7ac9e66e06617b6e8273f9e171fb8757cce93c48
St <aaronndas@gmail.com>
2024-09-18 16:04:57 +0200

    zero

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * this functions console.logs
+ * @param {string} msg - the message to print
+ * @return {void} - doesnt return anything
+ */
+
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
