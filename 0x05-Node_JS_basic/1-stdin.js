6379e31ecbb7dcfa100cdf687eecdcb47c3bbf6c
St <aaronndas@gmail.com>
2024-09-18 16:58:38 +0200

    two one

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,6 +7,8 @@ process.stdin.on('readable', () => {
 
   if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
-    process.stdout.write('This important software is now closing\n');
+    process.stdin.on('end', () => {
+      process.stdout.write('This important software is now closing\n');
+    });
   }
 });
